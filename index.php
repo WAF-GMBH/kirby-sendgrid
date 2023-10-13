c4e599e762c9e696449f803785acd40c539f889e
Malte Müller <malte@waf.gmbh>
2023-10-13 23:41:40 +0100

    Add support for file attachments

--- index.php
+++ index.php
@@ -3,15 +3,22 @@ class SendGridProvider extends Kirby\Email\Email {
     public function send(): bool {
       require('vendor/autoload.php');
       $email    = new \SendGrid\Mail\Mail();
-          
+      
+      // encode any attachments we might have
+      if ($this->attachments()) {
+          $encodedAttachment = base64_encode(file_get_contents($this->attachments()[0]));
+      }     
+      
       // we are only supporting one recipient per field
       $email->setFrom($this->from(), $this->fromName());
       $email->setSubject($this->subject());
       $email->addTo(array_key_first($this->to()));
-      if ($this->bcc() != null) {
-        $email->addBCC(array_key_first($this->bcc()));
-      }
+      $email->addBCC(array_key_first($this->bcc()));
       $email->addContent('text/plain', $this->body()->text());
+      
+      if ($this->attachments()) {
+        $email->addAttachment($encodedAttachment, 'text/CSV', 'order.csv', 'attachment');
+      }
                         
       // authenticate      
       $apiKey = getenv('SENDGRID_API_KEY'); 
@@ -32,7 +39,7 @@ class SendGridProvider extends Kirby\Email\Email {
     }
 }
 
-Kirby::plugin('waf-gmbh/sendgrid', [
+Kirby::plugin('my/sendgrid', [
     'components' => [
         'email' => function ($kirby, $props, $debug) {
             return new SendGridProvider($props, $debug);
